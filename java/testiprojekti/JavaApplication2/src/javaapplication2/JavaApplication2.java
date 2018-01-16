e0e9d759e26dc1351c1a91bf272df7a31d2bc0d4
matti1706 <35504550+matti1706@users.noreply.github.com>
2018-01-16 22:15:58 +0200

    test
    test

--- java/testiprojekti/JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ java/testiprojekti/JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -15,7 +15,7 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       System.out.println("Jukka PERKELE!!!"); // TODO code application logic here
     }
     
 }
