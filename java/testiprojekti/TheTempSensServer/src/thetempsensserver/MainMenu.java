bac5f9a650570acf42789b6a49839e0b7932e98f
JoakimHAMK <35504555+JoakimHAMK@users.noreply.github.com>
2018-02-17 21:35:03 +0200

    mainmenu

---
+++ java/testiprojekti/TheTempSensServer/src/thetempsensserver/MainMenu.java
@@ -0,0 +1,33 @@
+package thetempsensserver;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Joakim
+ */
+public class MainMenu {
+    
+    void MainMenu(){
+    
+    }
+    
+    public void SwitchCase(String pyynto){
+    
+    
+    switch (pyynto){
+        case "":
+        break;
+        
+    
+    
+    }
+    }
+    
+    
+    
+}
